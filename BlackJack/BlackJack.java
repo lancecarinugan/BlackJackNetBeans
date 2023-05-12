30e7a7abe7fcc89f625c1b44d5365f6c3df246b9
Lance Carinugan <lance.carinugan@mrjaffesclass.com>
2023-05-12 12:16:18 -0700

    Initial Commit

---
+++ BlackJack/BlackJack.java
@@ -0,0 +1,39 @@
+
+/**
+ * War game class
+ *
+ * @author Mr. Jaffe
+ * @version 2022-10-19
+ */
+public class BlackJack
+{
+    /**
+     * Constructor for the game
+     * Include your initialization here -- card decks, shuffling, etc
+     * Run the event loop after you've done the initializations
+     */
+    public BlackJack()
+    {
+        // Initializations here...
+        
+        
+        // ...then run the event loop
+        this.runEventLoop();
+    }
+    
+    /**
+     * This is the game's event loop. The code in here should come
+     * from the War flowchart you created for this game
+     */
+    public void runEventLoop() {
+
+    }
+    
+    /**
+     * The main method is called when Java starts your program
+     */
+    public static void main(String[] args) {
+        BlackJack bj = new BlackJack();
+    }
+
+}
